e0f39e723d8d778a64b64922c3bc6bf812094c60
WhiteCapella <whitecapella.official@gmail.com>
2024-09-26 11:32:30 +0900

    fix

--- src/hnh/app.py
+++ src/hnh/app.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 from transformers import pipeline
+import tensorflow as tf
 import os
 import json
-
+import random
 
 app = FastAPI()
 html = Jinja2Templates(directory="public")
@@ -14,28 +15,20 @@ html = Jinja2Templates(directory="public")
 async def create_upload_file(
     file: UploadFile
 ):
-    # save file
-    img = await file.read()
-    file_name = file.filename
-    file_ext = file.content_type.split('/')[-1]
+    try:
+        # save file
+        img = await file.read()
+        from PIL import Image
+        model = pipeline("image-classification", model = "julien-c/hotdog-not-hotdog")
+        img = Image.open(io.BytesIO(img))
+        prediction = model(img)
 
-    # if not exist dir, return error
-    # then, make dir
+        # 예측 결과 반환 (클라이언트가 이해하기 쉽도록)
+        return {"prediction": prediction[0]['label']}
 
-    upload_dir = os.path.expanduser("~/images/hnh/")
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
-    import uuid
-    from PIL import Image
-    file_full_path = os.path.join(upload_dir, f'{uuid.uuid4()}.{file_ext}')
-    with open(file_full_path, "wb") as f:
-        f.write(img)
-    
-    model = pipeline("image-classification", model = "julien-c/hotdog-not-hotdog")
-    img = Image.open(io.BytesIO(img))
-    p = model(img)
-
-    return {"Hello" : p}
+    except Exception as e:
+        # 예외 처리
+        return {"error": str(e)}
 
 @app.get("/")
 async def home(request: Request):

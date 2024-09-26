ba27e05aff95b7d81924d63ee2c77b0c85deafe7
WhiteCapella <whitecapella.official@gmail.com>
2024-09-26 09:43:40 +0900

    hello

--- src/hnh/app.py
+++ src/hnh/app.py
@@ -7,3 +7,25 @@ import os
 app = FastAPI()
 
 @app.get("/uploadfile")
+async def create_upload_file(
+    file: UploadFile
+):
+    # save file
+    img = await file.read()
+    file_name = file.filename
+    file_ext = file.content_type.split('/')[-1]
+
+    # if not exist dir, return error
+    # then, make dir
+
+    upload_dir = os.path.expanduser("~/images/hnh/")
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    import uuid
+    file_full_path = os.path.join(upload_dir, f'{uuid.uuid4()}.{file_ext}')
+    with open(file_full_path, "wb") as f:
+        f.write(img)
+
+@app.get("/predict")
+async def predict():
+    print("hello world!")
